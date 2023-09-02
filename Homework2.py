27c6257eefe6a927ae57e4065bbf2b8533fa796d
Melek DEDİK <melekdedik@gmail.com>
2023-09-02 16:22:43 +0300

    İlk Gün Ödev2 Kısımları

---
+++ Homework2.py
@@ -0,0 +1,12 @@
+krediler = ["Hızlı Kredi","Maaşını Halkbank'tan alanlara özel", "Mutlu emekli ihtiyaç kredisi"]
+
+print(krediler)
+print(krediler[0])
+print(krediler[1])
+
+print(len(krediler))
+
+krediler[0]="Çabuk Kredi"
+print(krediler)
+
+print(krediler[5])
---
+++ Homework2_1.py
@@ -0,0 +1,20 @@
+krediler = ["Hızlı Kredi","Maaşını Halkbank'tan alanlara özel", "Mutlu emekli ihtiyaç kredisi"]
+
+
+# o an gezilen değere alias denilir. Örn: kredi 
+for kredi in krediler:
+    print(kredi)
+
+
+# kredileri yazdırma
+for i in range(len(krediler)):
+    print(krediler[i])
+
+print("##################################################")
+
+for i in range (3,10):
+    print(i)
+
+print("##################################################")
+for i in range (0,11,2):
+    print(i)

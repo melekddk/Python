11bc197f483032b13bb7f0cb8ef09b84cafffcec
Melek DEDİK <melekdedik@gmail.com>
2023-09-02 15:53:04 +0300

    Pyton ile Selenium Eğitimi Ödev1 İşlemleri

---
+++ Homework1.py
@@ -0,0 +1,11 @@
+Programa_Dahil_Ol='Tebrikler! Kampa katılımınız sağlandı.'
+Programa_Dahil_Ol2='Zaten bir hesaplarınız mı var? Giriş yapmak'
+
+egitimeKatil=Programa_Dahil_Ol
+
+
+
+if(egitimeKatil!=Programa_Dahil_Ol):
+  print('Tebrikler! Kampa katılımınız sağlandı.')
+else:
+  print('Zaten bir hesaplarınız mı var? Giriş yapmak')
